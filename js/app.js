233dc5cc7f9e5c3aec3033144ab31c6fecf739e2
Abir Ashab Niloy <bsse1315@iit.du.ac.bd>
2024-07-02 16:52:15 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,3 @@
+function moreDescribe() { 
+    document.getElementById("hideDescribe").innerHTML = " I love problem solving and get dopamine from it as well.I want to work in real world projects with my problem solving and development skills, and always ready to learn new stuffs!";
+}
