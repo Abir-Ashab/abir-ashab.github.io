e69ee563789a22049105981b27b68b225a5e4e13
Abir Ashab Niloy <bsse1315@iit.du.ac.bd>
2024-09-07 17:28:46 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 function moreDescribe() { 
-    document.getElementById("hideDescribe").innerHTML = " My primary interests lie in Natural Language Processing (NLP) and Computer Vision (CV) where I am eager to dive deeper and make things more efficient.
-                Also as a MERN developer, I try to stay updated with the latest technologies and kinds of stuff.
-                I am currently seeking a research internship focused on NLP or CV to further expand my skills and increase the possibility of getting funding for a PhD or MS in the US.";
+    document.getElementById("hideDescribe").innerHTML = " My primary interests lie in Natural Language Processing (NLP) and Computer Vision (CV) where I am eager to dive deeper and make things more efficient. Also as a MERN developer, I try to stay updated with the latest technologies and kinds of stuff. I am currently seeking a research internship focused on NLP or CV to further expand my skills and increase the possibility of getting funding for a PhD or MS in the US.";
 }
